69c4a81432c47c97f04aa9de89ad4e75d4713689
Luis Ángel Méndez Gort <lamg@protonmail.com>
2024-05-19 13:13:49 +0200

    using falco instead of concatenating strings

--- web_generator/Cli/NotebookToHtml.fs
+++ web_generator/Cli/NotebookToHtml.fs
@@ -1,56 +1,62 @@
 module NotebookToHtml
 
+open System
 open System.IO
 open Newtonsoft.Json.Linq
 open Markdig
+
+open Fss
+open Falco.Markup
+
+let html, htmlNode = fss [ Custom "margin" "0"; Custom "padding" "0" ]
+
+let basePage body =
+  Elem.html
+    [ Attr.lang "en"; Attr.class' html ]
+    [ Elem.head
+        []
+        [ Elem.meta [ Attr.charset "UTF-8" ]
+          Elem.title [] [ Text.raw "LAMG" ]
+          Elem.link
+            [ Attr.href "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/default.min.css"
+              Attr.rel "stylesheet" ]
+          Elem.script [ Attr.src "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/highlight.min.js" ] []
+          Elem.script [] [ Text.raw "hljs.highlightAll();" ] ]
+      Elem.body [] body ]
+
+let extractCode (cell: JToken) =
+  let code = String.concat "\n" (cell.["source"].Values<string>())
+  Elem.pre [] [ Elem.code [ Attr.class' "language-fsharp" ] [ Text.raw (Web.HttpUtility.HtmlEncode code) ] ]
 
 let convert (inputPath: string) (outputPath: string) =
   // Read the Jupyter notebook JSON
   let notebookJson = File.ReadAllText inputPath
   let notebook = JObject.Parse(notebookJson)
 
-  // Create an HTML string
-  let htmlBuilder = System.Text.StringBuilder()
-  htmlBuilder.AppendLine("<html>") |> ignore
-  htmlBuilder.AppendLine("<head>") |> ignore
-  htmlBuilder.AppendLine("<title>Blog Post</title>") |> ignore
-
-  htmlBuilder.AppendLine(
-    "<link href=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/default.min.css\" rel=\"stylesheet\">"
-  )
-  |> ignore
-
-  htmlBuilder.AppendLine(
-    "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/highlight.min.js\"></script>"
-  )
-  |> ignore
-
-  htmlBuilder.AppendLine("<script>hljs.highlightAll();</script>") |> ignore
-  htmlBuilder.AppendLine("</head>") |> ignore
-  htmlBuilder.AppendLine("<body>") |> ignore
-
   // Extract the cells from the notebook
   let cells = notebook.["cells"].AsJEnumerable()
 
-  for cell in cells do
-    let cellType = cell.["cell_type"].Value<string>()
+  let html =
+    cells
+    |> Seq.choose (fun cell ->
+      let cellType = cell.["cell_type"].Value<string>()
 
-    match cellType with
-    | "markdown" ->
-      // Convert markdown to HTML
-      let markdown = String.concat "\n" (cell.["source"].Values<string>())
-      let html = Markdown.ToHtml(markdown)
-      htmlBuilder.AppendLine(html) |> ignore
-    | "code" ->
-      // Extract and highlight the F# code
-      let code = String.concat "\n" (cell.["source"].Values<string>())
-      htmlBuilder.AppendLine("<pre><code class=\"language-fsharp\">") |> ignore
-      htmlBuilder.AppendLine(System.Web.HttpUtility.HtmlEncode(code)) |> ignore
-      htmlBuilder.AppendLine("</code></pre>") |> ignore
-    | _ -> ()
+      match cellType with
+      | "markdown" ->
+        // Convert markdown to HTML
+        let markdown = String.concat "\n" (cell.["source"].Values<string>())
+        let html = Markdown.ToHtml(markdown)
+        Text.raw html |> Some
+      | "code" ->
+        // Extract and highlight the F# code
+        let code = String.concat "\n" (cell.["source"].Values<string>())
 
-  htmlBuilder.AppendLine("</body>") |> ignore
-  htmlBuilder.AppendLine("</html>") |> ignore
+        Elem.pre [] [ Elem.code [ Attr.class' "language-fsharp" ] [ Text.raw code ] ]
+        |> Some
+      | _ -> None)
+    |> Seq.toList
+    |> basePage
+    |> renderHtml
 
   // Write the HTML to the output file
-  File.WriteAllText(outputPath, htmlBuilder.ToString())
+  File.WriteAllText(outputPath, html)
--- web_generator/Cli/Program.fs
+++ web_generator/Cli/Program.fs
@@ -1,15 +1,21 @@
 ﻿open System
 open System.IO
 
+open Fss
+open Fss.Types
+open Falco.Markup
+
+let html, htmlNode = fss [ Custom "margin" "0"; Custom "padding" "0" ]
+
 [<Literal>]
 let indexHtml = "index.html"
 
 let createLink (filePath: string) =
   let fileName = Path.ChangeExtension(Path.GetFileName filePath, null)
-  sprintf "<li><a href=\"%s\">%s</a></li>" filePath fileName
+  Elem.li [] [ Elem.a [ Attr.href filePath ] [ Text.raw fileName ] ]
 
 let generate (inputDir: string) (outputDir: string) =
-  let ipynbFiles = Directory.GetFiles(inputDir, "*.ipynb")
+  let ipynbFiles = Directory.GetFiles(inputDir, "*.ipynb") |> Array.toList
   let outputPath = Path.Join(Environment.CurrentDirectory, outputDir)
 
   try
@@ -21,34 +27,21 @@ let generate (inputDir: string) (outputDir: string) =
 
   let htmlFiles =
     ipynbFiles
-    |> Array.map (fun f ->
+    |> List.map (fun f ->
       let htmlFile = Path.ChangeExtension(Path.GetFileName f, "html")
 
       NotebookToHtml.convert f (Path.Join(outputPath, htmlFile))
       htmlFile)
 
-  let links = htmlFiles |> Array.map createLink |> String.concat "\n"
+
+  let links = htmlFiles |> List.map createLink
 
   let htmlContent =
-    sprintf
-      """
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Index of Files</title>
-            </head>
-            <body>
-                <h1>Index of Files</h1>
-                <ul>
-                    %s
-                </ul>
-            </body>
-            </html>
-            """
-      links
-
+    Elem.html
+      [ Attr.lang "en"; Attr.class' html ]
+      [ Elem.head [] [ Elem.meta [ Attr.charset "UTF-8" ]; Elem.title [] [ Text.raw "LAMG" ] ]
+        Elem.body [] [ Elem.li [] links ] ]
+    |> renderHtml
 
   File.WriteAllText(Path.Join(outputPath, indexHtml), htmlContent)
 
